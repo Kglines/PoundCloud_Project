f04b5612b389b738745ba22861757ac7b26255f0
Keith Glines <keithglines@yahoo.com>
2022-07-18 17:29:52 -0400

    Update route

--- backend/routes/api/songs.js
+++ backend/routes/api/songs.js
@@ -24,7 +24,7 @@ router.get('/:songId', requireAuth, async (req, res) => {
             as: 'Artist',
             attributes: [
                 'id',
-                'userName',
+                'username',
                 'previewImage'
             ]
         }],
