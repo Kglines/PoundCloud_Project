e3cca5d71fe01bb28868a461f34e14060339b8e3
Keith Glines <keithglines@yahoo.com>
2022-07-21 18:51:22 -0400

    Edit comment

--- backend/routes/api/comments.js
+++ backend/routes/api/comments.js
@@ -2,11 +2,38 @@ const express = require('express');
 
 const router = express.Router();
 const { Comment } = require('../../db/models');
+const { requireAuth } = require('../../utils/auth');
+const { validateComment } = require('../../utils/validation');
 
 router.get('/', async (req, res) => {
     const comments = await Comment.findAll();
 
     res.json(comments);
+})
+
+// Edit a Comment
+router.put('/:commentId', [requireAuth, validateComment], async (req, res) => {
+    const { user } = req;
+    const { commentId } = req.params;
+    const { body } = req.body;
+
+    const comment = await Comment.findByPk(commentId);
+    if(comment){
+        if(comment.userId === user.id){
+            await comment.update({
+                body
+            });
+            res.json(comment)
+        } else {
+            const error = new Error("Unauthorized");
+            error.status = 403;
+            throw error;
+        }
+    } else {
+        const error = new Error("Comment couldn't be found");
+        error.status = 404;
+        throw error;
+    }
 })
 
 module.exports = router;
