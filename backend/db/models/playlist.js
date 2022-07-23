11e552a52044a2e573e237cfc375b51bc45263a5
Keith Glines <keithglines@yahoo.com>
2022-07-23 13:49:21 -0400

    Update delete playlist

--- backend/db/models/playlist.js
+++ backend/db/models/playlist.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Playlist.belongsToMany(models.Song, { through: models.PlaylistSong, foreignKey: 'playlistId' });
+      Playlist.belongsToMany(models.Song, { through: models.PlaylistSong, foreignKey: 'playlistId', otherKey: 'songId' });
       Playlist.belongsTo(models.User, { foreignKey: 'userId' });
     }
   }
