54919f72278a058e522825a2481ab33eaf3eda89
Keith Glines <keithglines@yahoo.com>
2022-08-29 20:54:12 -0400

    Update Current User

--- frontend/src/components/CurrentUser/index.js
+++ frontend/src/components/CurrentUser/index.js
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchAllSongs } from '../../store/songs';
 
 function CurrentUser() {
+    const dispatch = useDispatch();
+
+    const songs = Object.values(useSelector(state => state.songs));
+    const currentUser = useSelector(state => state.session.user);
+    console.log(songs.map(song => song.userId === currentUser.id));
+    const userSongs = [];
+
+    
+
+    useEffect(() => {
+        dispatch(fetchAllSongs())
+    }, [dispatch]);
+
   return (
     <div>CurrentUser</div>
   )
