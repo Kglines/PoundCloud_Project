adb5f5132608f97357efc78b0074371f0d4bd63d
Keith Glines <keithglines@yahoo.com>
2022-09-08 16:13:31 -0400

    Styling

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -35,7 +35,7 @@ function ProfileButton({ user }) {
   return (
     <>
       <button onClick={openMenu}>
-        <i className='fas fa-user-circle' />
+        <i className='fas fa-user-circle profile-btn' />
       </button>
       {showMenu && (
         <ul className='profile-dropdown'>
