e37ffcf717400442b646a34b2301f43f1efd180c
Keith Glines <keithglines@yahoo.com>
2023-02-13 18:46:51 -0500

    Library Page

--- frontend/src/components/Library/index.js
+++ frontend/src/components/Library/index.js
@@ -46,68 +46,92 @@ const Library = () => {
           <SearchBar />
         </div>
       </div>
-      <div className='song-container-home'>
-        {songs?.map((song) => {
-          return (
-            <div key={song?.id} className='song-card-home'>
-              <NavLink className='song-link-home' to={`/songs/${song?.id}`}>
-                <li className='song-item-home'>
-                  <div>
-                    <img
-                      className='song-img-home'
-                      src={song?.previewImage}
-                      alt={song?.title}
-                    />
-                    <h2 className='song-title-home'>{song?.title}</h2>
-                  </div>
-                  <p className='song-desc-home'>{song?.description}</p>
-                </li>
-              </NavLink>
-            </div>
-          );
-        })}
+      <div className='curr-user-summary-songs'>
+        <div className='user-song-header'>
+          <h3>
+            <NavLink to='/songs'>Songs:</NavLink>
+          </h3>
+        </div>
+        <div className='song-container-home'>
+          {songs?.map((song) => {
+            return (
+              <div key={song?.id} className='song-card-home'>
+                <NavLink className='song-link-home' to={`/songs/${song?.id}`}>
+                  <li className='song-item-home'>
+                    <div>
+                      <img
+                        className='song-img-home'
+                        src={song?.previewImage}
+                        alt={song?.title}
+                      />
+                      <h2 className='song-title-home'>{song?.title}</h2>
+                    </div>
+                    <p className='song-desc-home'>{song?.description}</p>
+                  </li>
+                </NavLink>
+              </div>
+            );
+          })}
+        </div>
       </div>
-      <div className='song-container-home'>
-        {albums?.map((album) => {
-          return (
-            <div key={album?.id} className='song-card-home'>
-              <NavLink className='song-link-home' to={`/albums/${album?.id}`}>
-                <li className='song-item-home'>
-                  <div>
-                    <img
-                      className='song-img-home'
-                      src={album?.previewImage}
-                      alt={album?.title}
-                    />
-                    <h2 className='song-title-home'>{album?.title}</h2>
-                  </div>
-                  <p className='song-desc-home'>{album?.description}</p>
-                </li>
-              </NavLink>
-            </div>
-          );
-        })}
+      <div className='curr-user-summary-albums'>
+        <div className='user-album-header'>
+          <h3>
+            <NavLink to='/albums'>Albums:</NavLink>
+          </h3>
+        </div>
+        <div className='song-container-home'>
+          {albums?.map((album) => {
+            return (
+              <div key={album?.id} className='song-card-home'>
+                <NavLink className='song-link-home' to={`/albums/${album?.id}`}>
+                  <li className='song-item-home'>
+                    <div>
+                      <img
+                        className='song-img-home'
+                        src={album?.previewImage}
+                        alt={album?.title}
+                      />
+                      <h2 className='song-title-home'>{album?.title}</h2>
+                    </div>
+                    <p className='song-desc-home'>{album?.description}</p>
+                  </li>
+                </NavLink>
+              </div>
+            );
+          })}
+        </div>
       </div>
-      <div className='song-container-home'>
-        {playlists?.map((playlist) => {
-          return (
-            <div key={playlist?.id} className='song-card-home'>
-              <NavLink className='song-link-home' to={`/playlists/${playlist?.id}`}>
-                <li className='song-item-home'>
-                  <div>
-                    <img
-                      className='song-img-home'
-                      src={playlist?.previewImage}
-                      alt={playlist?.title}
-                    />
-                    <h2 className='song-title-home'>{playlist?.name}</h2>
-                  </div>
-                  <p className='song-desc-home'>{playlist?.User?.username}</p>
-                </li>
-              </NavLink>
-            </div>
-          );
-        })}
+      <div className='curr-user-summary-playlist'>
+        <div className='user-playlist-header'>
+          <h3>
+            <NavLink to='/playlists'>Playlists:</NavLink>
+          </h3>
+        </div>
+        <div className='song-container-home'>
+          {playlists?.map((playlist) => {
+            return (
+              <div key={playlist?.id} className='song-card-home'>
+                <NavLink
+                  className='song-link-home'
+                  to={`/playlists/${playlist?.id}`}
+                >
+                  <li className='song-item-home'>
+                    <div>
+                      <img
+                        className='song-img-home'
+                        src={playlist?.previewImage}
+                        alt={playlist?.title}
+                      />
+                      <h2 className='song-title-home'>{playlist?.name}</h2>
+                    </div>
+                    <p className='song-desc-home'>{playlist?.User?.username}</p>
+                  </li>
+                </NavLink>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
