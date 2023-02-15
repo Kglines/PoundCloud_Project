ea694d1bb1f9005d03813c379fc4f0c72f5a7b0b
Keith Glines <keithglines@yahoo.com>
2023-02-14 19:30:07 -0500

    Edit song Modal

--- frontend/src/components/Songs/EditSong/index.js
+++ frontend/src/components/Songs/EditSong/index.js
@@ -54,68 +54,85 @@ function EditSong({ setShowEditModal }) {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h2>Edit your song</h2>
-      <ul>
-        {validationErrors.length > 0 && validationErrors.map(error => (
-          <li className='errors' key={error}>{error}</li>
-        ))}
-      </ul>
-      <label>
-        Title
-        <input
-          type='text'
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          name='title'
-          placeholder='Title'
-        />
-      </label>
-      <label>
-        Description
-        <input
-          type='text'
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-          name='description'
-          placeholder='Description'
-        />
-      </label>
-      {/* <label>
-        Song Url
-        <input
-          type='text'
-          value={url}
-          onChange={(e) => setUrl(e.target.value)}
-          name='url'
-          placeholder='Song url'
-        />
-      </label> */}
-      <label>
-        Image Url
-        <input
-          type='text'
-          value={imageUrl}
-          onChange={(e) => setImageUrl(e.target.value)}
-          name='imageUrl'
-          placeholder='Image url'
-        />
-      </label>
-      {/* <p style={{ opacity: 0.5}}>(Select the album to add your song to...)</p> */}
-      {/* {myAlbums.map((album) => (
-        <label key={album.id}>
+    <form className='create-song-form' onSubmit={handleSubmit}>
+      <div className='create-song-modal-header'>
+        <h2>Edit Your Song</h2>
+      </div>
+      <div className='create-song-container'>
+        <ul>
+          {validationErrors.length > 0 &&
+            validationErrors.map((error) => (
+              <li className='errors' key={error}>
+                {error}
+              </li>
+            ))}
+        </ul>
+        <label>
+          Title
           <input
-            type='button'
-            value={album.id}
-            onClick={(e) => setSelectedAlbumId(e.target.value)}
-            className='select-album-input'
-          ></input>
-          {album.title}
+            type='text'
+            className='create-song-input'
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            name='title'
+            placeholder='Title'
+          />
         </label>
-      ))} */}
-      <div>
-        <button className='submit-btn' type='submit'>Submit</button>
-        <button className='cancel-btn' onClick={() => setShowEditModal(false)}>Cancel</button>
+        <label>
+          Description
+          <input
+            type='text'
+            className='create-song-input'
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            name='description'
+            placeholder='Description'
+          />
+        </label>
+        {/* <label>
+          Song Url
+          <input
+            type='text'
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+            name='url'
+            placeholder='Song url'
+          />
+        </label> */}
+        <label>
+          Image Url
+          <input
+            type='text'
+            className='create-song-input'
+            value={imageUrl}
+            onChange={(e) => setImageUrl(e.target.value)}
+            name='imageUrl'
+            placeholder='Image url'
+          />
+        </label>
+        {/* <p style={{ opacity: 0.5}}>(Select the album to add your song to...)</p> */}
+        {/* {myAlbums.map((album) => (
+          <label key={album.id}>
+            <input
+              type='button'
+              value={album.id}
+              onClick={(e) => setSelectedAlbumId(e.target.value)}
+              className='select-album-input'
+            ></input>
+            {album.title}
+          </label>
+        ))} */}
+        <div>
+          <button className='submit-btn submit-song' type='submit'>
+            Submit
+          </button>
+          <button
+            className='cancel-btn cancel-song'
+            onClick={() => setShowEditModal(false)}
+          >
+            Cancel
+          </button>
+        </div>
       </div>
     </form>
   );
