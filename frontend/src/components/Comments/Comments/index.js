46de246f480046823dcc5acb9832084612cef911
Keith Glines <keithglines@yahoo.com>
2024-01-03 19:17:08 -0500

    Fixed Comment styling

--- frontend/src/components/Comments/Comments/index.js
+++ frontend/src/components/Comments/Comments/index.js
@@ -1,44 +1,47 @@
-import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { fetchGetComments } from '../../../store/comments'
-import Comment from '../Comment/Comment'
-import './Comments.css'
-
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchGetComments } from '../../../store/comments';
+import Comment from '../Comment/Comment';
+import './Comments.css';
 
 function Comments({ song }) {
-    const dispatch = useDispatch()
-    const [comments, setComments] = useState(null)
-    const [errors, setErrors] = useState([])
+  const dispatch = useDispatch();
+  const [comments, setComments] = useState(null);
+  const [errors, setErrors] = useState([]);
 
-    const commentsArr = useSelector(state => state.comments)
-    
-    useEffect(() => {
-      const fetchData = async () => {
-        const comments = await dispatch(fetchGetComments(song?.id));
-        setComments(comments);
-        };
-        fetchData()
-          .catch(async (res) => {
-            const data = res.json();
-            if (data && data.errors) setErrors(data.errors);
-          });
-    }, [dispatch, song?.id])
+  const commentsArr = useSelector((state) => state.comments);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const comments = await dispatch(fetchGetComments(song?.id));
+      setComments(comments);
+    };
+    fetchData().catch(async (res) => {
+      const data = res.json();
+      if (data && data.errors) setErrors(data.errors);
+    });
+  }, [dispatch, song?.id]);
 
   return (
     <div className='comments-container'>
-    <div>
-      <p className='comments-total'>{commentsArr?.Comments?.length} comments</p>
-    </div>
-    <div>
-
-      {commentsArr?.Comments?.map((comment) => (
-        <div key={comment?.id}>
-          <Comment comment={comment} song={song} user={comment?.User?.username} />
-        </div>
-      ))}
-    </div>
+      <div>
+        <p className='comments-total'>
+          {commentsArr?.Comments?.length} comments
+        </p>
+      </div>
+      <div className='comments-arr'>
+        {commentsArr?.Comments?.map((comment) => (
+          <div key={comment?.id}>
+            <Comment
+              comment={comment}
+              song={song}
+              user={comment?.User?.username}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
 
-export default Comments
+export default Comments;
