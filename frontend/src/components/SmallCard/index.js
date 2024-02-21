9fcfbc8083dcf1c8e8258cd8c3d4ed073db93ad4
Keith Glines <keithglines@yahoo.com>
2024-02-21 18:33:37 -0500

    small card setup

--- frontend/src/components/SmallCard/index.js
+++ frontend/src/components/SmallCard/index.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
 import './SmallCard.css';
 
 function SmallCard({ title, description, previewImage, id, url }) {
   return (
-    <section>
-      <div></div>
+    <section className='small-card-container'>
+      <NavLink className='small-card-link' key={id} to={`/${url}/${id}`}>
+        <div className='small-card-banner'>
+          <img src={previewImage} alt={title} className='small-card-image' />
+          <h3 className='small-card-title'>{title}</h3>
+        </div>
+      </NavLink>
+      <p className='small-card-description'>{description}</p>
     </section>
   );
 }
